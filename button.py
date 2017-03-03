f13a5878dcaa9e7b3abf6c38808ec3188128086e
Chander <luckynegi002@gmail.com>
2017-03-03 22:00:24 +0530

    all files added

---
+++ button.py
@@ -0,0 +1,31 @@
+from Tkinter import *
+"""class App:
+  def __init__(self, master):
+    frame = Frame(master)
+    frame.pack()
+    self.button = Button(frame, 
+                         text="QUIT", fg="red",
+                         command=frame.quit)
+    self.button.pack(side=LEFT)
+    self.slogan = Button(frame,
+                         text="Hello",
+                         command=self.write_slogan)
+    self.slogan.pack(side=LEFT)
+  def write_slogan(self):
+    print "Tkinter is easy to use!"
+
+root = Tk()
+app = App(root)
+root.mainloop()"""
+import Tkinter
+import tkMessageBox
+
+top = Tkinter.Tk()
+
+def helloCallBack():
+   tkMessageBox.showinfo( "Hello Python", "Hello World")
+
+B = Tkinter.Button(top, text ="Hello", command = helloCallBack)
+
+B.pack()
+top.mainloop()
